284d8d528c9833bfbd3a36abbb16fbb2078aa3dc
Nuno Ferreira <nuno.ferreira@gamesinc.co.uk>
2018-07-17 22:15:16 +0100

    added ability to save/read from disk

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -13,15 +13,52 @@ defmodule Cards do
 
   """
   def create_deck do
-      ["Ace", "Two", "Three", "Four", "Five", "Six"]
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+         "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
-     Enum.shuffle(deck)
+    Enum.shuffle(deck)
   end
 
   def contains?(deck, card) do
-      Enum.member?(deck, card)
+    Enum.member?(deck, card)
   end
 
+  def deal(deck, hand_size) do
+      Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+      binary = :erlang.term_to_binary(deck)
+      File.write(filename, binary)
+  end
+
+  def load filename do
+      case File.read filename do
+          {:ok, binary } -> :erlang.binary_to_term binary
+         { :error, _reason} -> "That file does not exist"
+      end
+
+
+  end
 end
